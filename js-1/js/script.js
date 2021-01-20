15d49389c307f2cf280a5bfed2665495d3865d5c
Burbuletka <burbuletka5@gmail.com>
2021-01-20 13:36:50 +0200

    js

---
+++ js-1/js/script.js
@@ -0,0 +1,26 @@
+// const
+
+const str = 'Привіт';
+
+const number = 123;
+
+const txt = 'true';
+
+const flag = true;
+
+const yearOfBirth = 2006;
+// let
+let a;
+console.log(typeof a); //undefined
+let b = 5;
+console.log(typeof b); // number
+let c = null;
+console.log(typeof c); //object
+let d = false;
+console.log(typeof d); // boolean
+let e = 'js is good';
+console.log(typeof e); //string
+
+
+
+
