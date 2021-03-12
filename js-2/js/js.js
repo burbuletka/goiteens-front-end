f1a59c06e668e90f3ddaba55bc03fcd895206b5c
Burbuletka <burbuletka5@gmail.com>
2021-03-12 20:57:46 +0200

    js-2

---
+++ js-2/js/js.js
@@ -0,0 +1,42 @@
+let name = prompt("Введіть ім'я");
+console.log(name);
+
+let age1 = prompt("Введіть вік друга");
+let age2 = prompt("Введіть свій вік");
+alert(age1 + age2);
+
+const a = 2
+const b = 3
+console.log("a + b =", a + b);
+
+let a1 = 5 + 3
+let a2 = 5 - 3
+let a3 = 5 * 3
+let a4 = 5 / 3
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+let a6 = 5 % 3
+let a7 = 3 % 5
+let a8 = 5 + '3'
+let a9 = "5" + 3
+let a10 = 75 + 'кг'
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+const height = 23 + 'cm'
+const width = 10 + 'cm'
+const S = height * width
+const п = 3.14
+console.log('S=', S);
+
+const heightC = 10 + 'm'
+const dC = 4 + 'm'
+const v = п * heightC * heightC * dC
+console.log(v)
